983e9cd3d299e63db6cfff79b34389bcf0cf58ac
algo1337 <bangod1337@gmail.com>
2024-10-11 17:30:27 +0000

    Documentation comments

--- map.h
+++ map.h
@@ -16,9 +16,37 @@ typedef struct Map {
 	void	(*Destruct)	(struct Map *m);
 } Map;
 
-Map NewMap();
-int InMap(Map *m, const char *q);
-Key *GetKey(Map *m, const char *q);
-char *GetKeyValue(Map *m, const char *q);
-int AppendKey(Map *m, const char *k, const char *v);
-void DestroyMap(Map *m);
+//
+//			| - > Create a new instanse of Map
+//			| - > Returns a an empty map
+//
+Map 		NewMap();
+
+//
+//			| - > Check if a key exists in the Map
+//			| - > Returns > 0 upon success or -1 upon failure
+//
+int 		InMap(Map *m, const char *q);
+
+//
+//			| - > Get a key struct with key information
+//			| - > Returns key upon success or NULL upon failure
+//
+Key 		*GetKey(Map *m, const char *q);
+
+//
+//			| - > Get a key's value
+//			| - > Returns the value upon success or NULL upon failure
+//
+char 		*GetKeyValue(Map *m, const char *q);
+
+//
+//			| - > Append a key with value to the map
+//			| - > Returns 1 upon success and 0 upon failure
+//
+int 		AppendKey(Map *m, const char *k, const char *v);
+
+//
+// 			| - > Destruct the struct
+//
+void 		DestroyMap(Map *m);
